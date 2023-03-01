7537417f4b0b368ba786a35209e7cf1018612aae
Gwiza Betty <123150054+Gwizabetty@users.noreply.github.com>
2023-03-01 09:39:07 +0200

    main

---
+++ App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Branch b = new Branch("Rulindo", "base", "base@hotmail.yaho.googlre");
+        Bank bpr = new Bank();
+        System.out.println(bpr);
+        // System.out.println();
+        System.out.println(b);
+    }
+}
