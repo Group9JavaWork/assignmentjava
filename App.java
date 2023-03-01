343daf611122a38c4d6c192999404169c637b736
Gwiza Betty <123150054+Gwizabetty@users.noreply.github.com>
2023-03-01 11:31:12 +0200

    Update App.java

--- App.java
+++ App.java
@@ -1,9 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Branch b = new Branch("Rulindo", "base", "base@hotmail.yaho.googlre");
-        Bank bpr = new Bank();
-        System.out.println(bpr);
-        // System.out.println();
-        System.out.println(b);
+        Branch branch1 = new Branch(" Equity Rwanda ", " Kigali-Rwanda ", " equityrwanda@gmail.com ");
+        Bank mybank = new Bank("Equity","Kenya","equity@gmail.com");
+        
+        System.out.println(mybank);
+        System.out.println();
+        System.out.println(branch1);
+        System.out.println();
     }
 }
