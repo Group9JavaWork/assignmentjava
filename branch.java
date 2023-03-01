d52dc703c487469c3da5db6c6921eb3149895f96
Gwiza Betty <123150054+Gwizabetty@users.noreply.github.com>
2023-03-01 18:24:13 +0200

    Update branch.java

--- branch.java
+++ branch.java
@@ -1,39 +1,49 @@
 import java.util.*;
+
 class branch {
     private String branchname;
     private String location;
     private String email;
+    private int listacc;
+
+    // first constructor
+    public branch() {
+
+    }
+
+    // second constructor
     public branch(String branchname, String location, int listacc, String email) {
         this.branchname = branchname;
         this.location = location;
         this.listacc = listacc;
         this.email = email;
     }
-    //secodnary constructor
-    public branch(){
 
-    }
     public String getBranchname() {
         return branchname;
     }
+
     public void setBranchname(String branchname) {
-       // Scanner branch=new Scanner(Systrem.in);
+        // Scanner branch=new Scanner(Systrem.in);
         this.branchname = branchname;
     }
+
     public String getLocation() {
         return location;
     }
+
     public void setLocation(String location) {
-        
+
         this.location = location;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
-        
+
         this.email = email;
     }
-    
 
 }
